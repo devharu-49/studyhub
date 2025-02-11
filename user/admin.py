edb7488c7e4a528a6aaa8b0c08795badbbedc545
mal-key <malulu620@gmail.com>
2025-02-11 19:15:25 +0900

    Tasksモデルをuserからtodoに移動

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser, Tasks, Times
+from .models import CustomUser, Times
 from django.contrib.auth.admin import UserAdmin
 
 
@@ -30,5 +30,5 @@ class CustomUserAdmin(admin.ModelAdmin):
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
-admin.site.register(Tasks)
+
 admin.site.register(Times)
--- user/models.py
+++ user/models.py
@@ -59,26 +59,6 @@ class CustomUser(AbstractUser):
         return self.name
 
 
-class Tasks(models.Model):
-
-    id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(
-        "CustomUser", on_delete=models.CASCADE
-    )  # Userが削除されたら削除
-    title = models.CharField(max_length=255, null=False, blank=False)
-    description = models.TextField()
-    is_completed = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
-    deadline = models.DateTimeField(null=True)
-
-    class Meta:
-        verbose_name_plural = "Todos"
-
-    def __str__(self):
-        return self.title
-
-
 class Times(models.Model):
 
     id = models.AutoField(primary_key=True)
