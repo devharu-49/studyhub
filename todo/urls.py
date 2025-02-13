57d6b548db47ab1aa0f0eb54c8087a49e2e18321
mal-key <malulu620@gmail.com>
2025-02-13 19:30:28 +0900

    Todo詳細取得機能作成

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import tasklist_view
+from .views import tasklist_view, taskdetail_view
 
 urlpatterns = [
     path("", tasklist_view, name="tasklist"),
+    path("<int:id>/", taskdetail_view, name="taskdetail"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Tasks
 
 
+# タスク一覧取得
 def tasklist_view(request):
-    tasks = Tasks.objects.filter(user=request.user)
+    # 現在ログインしているユーザーに関連するタスクを選択
+    # select_relatedを使って、tasksに関連するuser情報も一度に取得する（クエリ数を減らすため）
+    tasks = Tasks.objects.select_related("user").filter(user=request.user)
     return render(request, "todo.html", {"tasks": tasks})
+
+
+# タスク詳細取得
+def taskdetail_view(request, id):
+    # Tasksテーブルからidが一致し、かつuserがログイン中のユーザーのタスクを取得する。
+    # データが存在しない場合は自動的に「404エラー」
+    task = get_object_or_404(Tasks, id=id, user=request.user)
+    return render(request, "tododetail.html", {"task": task})
