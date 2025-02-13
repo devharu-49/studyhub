b3a3245e17bf513857c9493fb6fb442e29cb2d2b
mal-key <malulu620@gmail.com>
2025-02-13 22:53:31 +0900

    Todo作成機能作成

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import tasklist_view, taskdetail_view
+from .views import tasklist_view, taskdetail_view, taskcreate_view
+
 
 urlpatterns = [
-    path("", tasklist_view, name="tasklist"),
-    path("<int:id>/", taskdetail_view, name="taskdetail"),
+    path("", tasklist_view, name="task_list"),
+    path("<int:id>/", taskdetail_view, name="task_detail"),
+    path("form/", taskcreate_view, name="task_create"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Tasks
+from .forms import TaskForm
 
 
 # タスク一覧取得
@@ -16,3 +17,23 @@ def taskdetail_view(request, id):
     # データが存在しない場合は自動的に「404エラー」
     task = get_object_or_404(Tasks, id=id, user=request.user)
     return render(request, "tododetail.html", {"task": task})
+
+
+# タスク作成
+def taskcreate_view(request):
+    if not request.user.is_authenticated:
+        return redirect("login")  # 未ログインの場合はログインページにリダイレクト
+
+    if request.method == "POST":  # フォームがPOSTで送信されたとき
+        form = TaskForm(request.POST)  # フォームにPOSTデータを渡してインスタンス作成
+        if form.is_valid():  # フォームのバリデーションが成功した場合
+            task = form.save(commit=False)  # データベースに保存
+            task.user = request.user  # 現在ログインしているユーザーをタスクに関連付け
+            task.save()
+            return redirect("task_list")  # 一覧ページにリダイレクト
+        else:
+            return render(request, "todoform.html", {"form": form})
+    else:
+        form = TaskForm()  # フォームが送信されていない場合、空のフォームを表示
+
+    return render(request, "todoform.html", {"form": form})
